dc4abc7f8c6d72a05bb5ae6ec0762803d0c20d66
Anri Lombard <anri.m.lombard@gmail.com>
2022-02-17 19:53:35 +0200

    Functions progress in perculation

--- PercolationStats.java
+++ PercolationStats.java
@@ -2,11 +2,19 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * The {@code PercolationStats} class provides methods
+ * for computing percolation threshold.
+ *
+ * @author Anri Lombard
+ */
 public class PercolationStats {
-    private double mean;
-    private double stddev;
-    private double confidenceLo_value;
-    private double confidenceHi_value;
+    private final int trials; // trials - number of observations in the experiment
+    private final double[] arrFractionOfOpenSites;
+    // see: https://en.wikipedia.org/wiki/1.96
+    private static final double PART_OF_STANDARD_DEVIATION = 1.96;
+    private final double mean;
+    private final double stddev;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
@@ -15,23 +23,57 @@ public class PercolationStats {
         }
     }
 
-    // sample mean of percolation threshold
+     /**
+     * Returns a sample mean of percolation threshold.
+     * Provides an estimate of the percolation threshold.
+     *
+     * @return a sample mean of percolation threshold.
+     */
     public double mean() {
-        
+        return mean;
     }
 
-    // sample standard deviation of percolation threshold
-    public double stddev()
+     /**
+     * Sample standard deviation of percolation threshold.
+     * It measures the sharpness of the threshold.
+     *
+     * @return standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return stddev;
+    }
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    /**
+     * Returns a low endpoint of 95% confidence interval.
+     *
+     * @return a low endpoint of 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - PART_OF_STANDARD_DEVIATION * stddev() / Math.sqrt(trials);
+    }
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    /**
+     * Returns a high endpoint of 95% confidence interval.
+     *
+     * @return a right value from the confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + PART_OF_STANDARD_DEVIATION * stddev() / Math.sqrt(trials);
+    }
 
-   // test client (see below)
+   /**
+     * Test client for PercolationStats.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
-        int input1 = Integer.parseInt(args[0]);
-        int input2 = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats newPercStats = new PercolationStats(n, trials);
+        StdOut.println("mean                    = " + newPercStats.mean());
+        StdOut.println("stddev                  = " + newPercStats.stddev());
+        StdOut.println("95% confidence interval = ["
+                + newPercStats.confidenceLo() + ", "
+                + newPercStats.confidenceHi() + "]");
     } 
 }
