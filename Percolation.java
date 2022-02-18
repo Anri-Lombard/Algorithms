a5edcc5b042c7ef34f343c52238b1e19a93b2236
Anri Lombard <anri.m.lombard@gmail.com>
2022-02-18 13:14:56 +0200

    Understanding percolation

--- Percolation.java
+++ Percolation.java
@@ -16,7 +16,12 @@ public class Percolation {
     private int n;
     private int openSites;
 
-    // creates n-by-n grid, with all sites initially blocked
+    /**
+     * Creates n-by-n grid, with all sites initially blocked.
+     * Performance: time proportional to  4n =  2n + 2n.
+     *
+     * @param n width of the grid
+     */
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
 
@@ -27,22 +32,61 @@ public class Percolation {
         openSites = 0;
     }
 
-    // opens the site (row, col) if it is not open already
+    /**
+     * Opens the site (row, col) if it is not open already.
+     * Performance: constant time plus a constant number of calls to the uf method union()
+     * We have 2 * 4 = 8 calls to the union() method in the worst case.
+     * Notice that row and col are counted from 1 to n.
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     */
     public void open(int row, int col)
 
-    // is the site (row, col) open?
+    /**
+     * Returns a boolean value which is true when the site (row, col) is open
+     * and returns false otherwise.
+     * Performance: constant time.
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     * @return {@code true} if the site is full, {@code false} otherwise
+     */
     public boolean isOpen(int row, int col)
 
-    // is the site (row, col) full?
+    /**
+     * Returns a boolean value which is true when the site (row, col) is full
+     * (flooded with water / painted blue) and returns false otherwise.
+     * Performance: 1 call to the uf method connected() in the worst case.
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     * @return {@code true} if the site is full, {@code false} otherwise
+     */
     public boolean isFull(int row, int col)
 
-    // returns the number of open sites
+    /**
+     * Returns the number of open sites.
+     * Performance: constant time.
+     *
+     * @return an integer value of the number of open sites.
+     */
     public int numberOfOpenSites()
 
-    // does the system percolate?
+    /**
+     * Returns a boolean value which is true when the system percolates
+     * and false otherwise.
+     * Performance: only 1 call to the uf method connected().
+     *
+     * @return {@code true} if the system percolates, {@code false} otherwise
+     */
     public boolean percolates()
 
-    // test client (optional)
+    /**
+     * Test client for Percolation.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         
         
